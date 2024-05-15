0685d6e72ea9dfa839ea9616d22e538cac49b149
Sakshyam Patro <112191154+Sakshyam-Patro@users.noreply.github.com>
2024-05-15 22:54:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+//scroll sections
+
+//scroll sections
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            document.querySelector('header nav a[href="#' + id + '"]').classList.add('active');
+        }
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
